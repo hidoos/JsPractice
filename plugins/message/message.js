13bc74e83280b7b796cc3bfc43ac6ab84435b5b0
hidoos <sk8huaban@gmail.com>
2014-12-09 00:45:57 +0800

    添加utils函数

--- plugins/message/message.js
+++ plugins/message/message.js
@@ -1,3 +1,50 @@
 /**
  * Created by hidoos on 2014/12/8.
  */
+(function(global){
+    var container,status;
+
+    var utils = {
+        isType:function(obj,type) {
+            return Object.prototype.toString.call(obj).slice(8, -1) === "[Object " + type + "]"
+        },
+        extend : function(){
+
+        }
+    }
+
+
+
+    var Message = function(){
+        this.container = document.createElement("div")
+        this.container.className = "message"
+
+        this.defaults = {
+            status : "normal",
+            placement : "top"
+        }
+        this.closeEl = "<span class='close'>&times;</span>";
+
+        return this;
+    }
+
+    Message.prototype.init = function(){
+        this.container.appendChild(this.closeEl)
+    }
+
+    Message.prototype.hide = function(){
+
+    }
+
+    Message.prototype.show = function(){
+        this.init();
+        container.innerHTML = "测试一下内容"
+        var body = document.body;
+        body.appendChild(container);
+    }
+
+    // export window
+    global.message = new Message();
+
+})(this);
+
